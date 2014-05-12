5ed485be5004912bc12592b8796daeb1818d0aea
EdwGx <edguo98@gmail.com>
2014-05-12 19:00:36 -0400

    Add sky map class

---
+++ AttackHelicopter/SkyMap.m
@@ -0,0 +1,13 @@
+//
+//  SkyMap.m
+//  AttackHelicopter
+//
+//  Created by edguo on 2014-05-12.
+//  Copyright (c) 2014 Edward Peiliang Guo. All rights reserved.
+//
+
+#import "SkyMap.h"
+
+@implementation SkyMap
+
+@end
